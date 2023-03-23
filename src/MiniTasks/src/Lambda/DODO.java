a604bf219de480a4791e5fe09a4bc066292d57fb
satomi20 <95304956+satomi20@users.noreply.github.com>
2023-03-23 10:19:22 +0300

    Update DODO.java

--- src/MiniTasks/src/Lambda/DODO.java
+++ src/MiniTasks/src/Lambda/DODO.java
@@ -3,10 +3,10 @@ package MiniTasks.src.Lambda;
 public class DODO {
     public static void main(String[] args) {
         Bob bob = (a, b) -> a + " " + b;
-        String res = bob.plus("Hello", "Nigga");
+        String res = bob.plus("Hello", "wigga");
         System.out.println(res);
         GOG gog = a -> a.substring(3);
-        String ef = gog.str("My nigga??///");
+        String ef = gog.str("My wigga??///");
         System.out.println(ef);
 
 
